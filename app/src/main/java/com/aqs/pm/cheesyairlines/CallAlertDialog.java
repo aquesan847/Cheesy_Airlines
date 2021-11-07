d97a0075f4a5536991e309a7886a6b88a4585268
aquesan847 <aquesada13@ieszaidinvergeles.org>
2021-11-07 21:04:33 +0100

    Project about an airline finished with all methods and a stylish AlertDialog.
    Alvaro Quesada Sanchez.

--- app/src/main/java/com/aqs/pm/cheesyairlines/CallAlertDialog.java
+++ app/src/main/java/com/aqs/pm/cheesyairlines/CallAlertDialog.java
@@ -1,34 +1,54 @@
 package com.aqs.pm.cheesyairlines;
 
+import android.app.Activity;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.app.AlertDialog;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.fragment.app.DialogFragment;
 
-public class CallAlertDialog extends DialogFragment {
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+public class CallAlertDialog {
 
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setTitle("You have chosen the normal buy option.")
-                .setMessage("Are you sure about this?")
-                .setPositiveButton("Yes",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.cancel();
-                            }
-                        })
-                .setNegativeButton("No",
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                dialogInterface.cancel();
-                            }
+    public void showDialog(Activity activity) {
+        Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setCancelable(false);
+        dialog.setContentView(R.layout.layout_alert_dialog);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
+
+        FloatingActionButton floatActionCancel = dialog.findViewById(R.id.cancel);
+        floatActionCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.cancel();
+            }
         });
 
-        return builder.create();
+        FloatingActionButton floatActionBuy = dialog.findViewById(R.id.buy);
+        floatActionBuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(activity,"Bought!" ,Toast.LENGTH_SHORT).show();
+                dialog.cancel();
+            }
+        });
+
+        dialog.show();
     }
+
 
 }
--- app/src/main/java/com/aqs/pm/cheesyairlines/MainActivity.java
+++ app/src/main/java/com/aqs/pm/cheesyairlines/MainActivity.java
@@ -3,14 +3,18 @@ package com.aqs.pm.cheesyairlines;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.PopupMenu;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -43,6 +47,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Switch switchMobility = findViewById(R.id.switch_mobility);
         switchMobility.setOnCheckedChangeListener(this);
+
+        CheckBox cbPet = findViewById(R.id.cbPet);
+        cbPet.setOnCheckedChangeListener(this);
+
+        CheckBox cbFirstClass = findViewById(R.id.cbFirstClass);
+        cbFirstClass.setOnCheckedChangeListener(this);
     }
 
     public void showPopup(View v) {
@@ -58,12 +68,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.btBuy:
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                CallAlertDialog dialogBuy = new CallAlertDialog();
-                dialogBuy.show(fragmentManager, "Dialog Alert");
+                CallAlertDialog alert = new CallAlertDialog();
+                alert.showDialog(MainActivity.this);
                 break;
             case R.id.imgFav:
-                //Toast.makeText(this, "Favorite", Toast.LENGTH_SHORT).show();
+                //If you press the heart icon at ActionBar it will display a Toast with a design.
                 LayoutInflater inflater = getLayoutInflater();
                 View layout = inflater.inflate(R.layout.toast_fav,
                         (ViewGroup) findViewById(R.id.toast_layout_root));
@@ -76,49 +85,49 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 toast.show();
                 break;
 
-            /*case R.id.cbFirstClass:
-                LayoutInflater inflater2 = getLayoutInflater();
-                View layout2 = inflater2.inflate(R.layout.toast_fav,
-                        (ViewGroup) findViewById(R.id.toast_layout_root));
-                TextView text2 = (TextView) layout2.findViewById(R.id.text);
-                text2.setText("First class set");
-
-                Toast toast2 = new Toast(getApplicationContext());
-                toast2.setDuration(Toast.LENGTH_SHORT);
-                toast2.setView(layout2);
-                toast2.show();
-                    break;
-
-            case R.id.cbPet:
-                LayoutInflater inflater3 = getLayoutInflater();
-                View layout3 = inflater3.inflate(R.layout.toast_fav,
-                        (ViewGroup) findViewById(R.id.toast_layout_root));
-                TextView text3 = (TextView) layout3.findViewById(R.id.text);
-                text3.setText("Pet set");
-
-                Toast toast3 = new Toast(getApplicationContext());
-                toast3.setDuration(Toast.LENGTH_SHORT);
-                toast3.setView(layout3);
-                toast3.show();
-                break;*/
-
             case R.id.imgBtBuy:
+                //Snackbar with design. I changed the text color to my cheesy yellow set at colors.xml
                 Snackbar snackbarBuyNow = Snackbar.make(view, R.string.buy_now_snack, BaseTransientBottomBar.LENGTH_INDEFINITE);
-                snackbarBuyNow.setAction(R.string.buy_now_error, new MyUndoListener());
+                snackbarBuyNow.setActionTextColor(getResources().getColor(R.color.cheesy_yellow))
+                        .setAction(R.string.buy_now_error, new MyUndoListener());
                 snackbarBuyNow.show();
                 break;
         }
     }
 
     @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
 
         switch (compoundButton.getId()){
-            case R.id.switch_mobility:
-                System.out.println("Switch");
-                break;
             case R.id.cbPet:
-                System.out.println("Pet");
+                //If you press the checkbox of "Pet" a toast will be displayed with a paw icon
+                if(isChecked) {
+                    LayoutInflater inflater = getLayoutInflater();
+                    View layout = inflater.inflate(R.layout.toast_pet,
+                            (ViewGroup) findViewById(R.id.toast_layout_root));
+                    TextView text = (TextView) layout.findViewById(R.id.text);
+                    text.setText("Pet");
+
+                    Toast toast = new Toast(getApplicationContext());
+                    toast.setDuration(Toast.LENGTH_SHORT);
+                    toast.setView(layout);
+                    toast.show();
+                }
+                break;
+            case R.id.cbFirstClass:
+                if (isChecked) {
+                //If you press the checkbox of "First class" a toast will be displayed with a medal icon
+                    LayoutInflater inflater2 = getLayoutInflater();
+                    View layout2 = inflater2.inflate(R.layout.toast_first_class,
+                            (ViewGroup) findViewById(R.id.toast_layout_root));
+                    TextView text2 = (TextView) layout2.findViewById(R.id.text);
+                    text2.setText("First class set");
+
+                    Toast toast2 = new Toast(getApplicationContext());
+                    toast2.setDuration(Toast.LENGTH_SHORT);
+                    toast2.setView(layout2);
+                    toast2.show();
+                }
                 break;
         }
     }
